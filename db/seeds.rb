2250212c24709b41632cc47611b008c07720ad5f
MLuus <morne.luus@gmail.com>
2014-07-16 17:38:49 +0800

    adding cityurl to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Kempinski',
   tfd: 90
+  hotelurl:'http://www.kempinski.com/en/bangkok/siam-hotel/welcome/'
+  cityurl:
 )
 
 Itinerary.create(
@@ -22,6 +24,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Tapei Royal Hotel',
   tfd: 90
+  hotelurl: 'http://www.taipeiroyalhotel.com/'
+  cityurl:'http://en.wikipedia.org/wiki/Taipei'
 )
 
 Itinerary.create(
@@ -31,6 +35,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Vendome East Hotel',
   tfd: 90
+  hotelurl:'http://vendome-hotel-taipei.h-rez.com'
+  cityurl:'http://en.wikipedia.org/wiki/Taipei'
 )
 
 Itinerary.create(
@@ -40,6 +46,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Mulan Hotel',
   tfd: 90
+  hotelurl:'http://www.swmall.com'
+  cityurl:'http://en.wikipedia.org/wiki/Taipei'
 )
 
 Itinerary.create(
@@ -49,6 +57,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Madison Taipei Hotel',
   tfd: 90
+  hotelurl:'http://www.madisontaipei.com/'
+  cityurl:'http://en.wikipedia.org/wiki/Taipei'
 )
 
 Itinerary.create(
@@ -58,6 +68,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'W Taipei Hotel',
   tfd: 90
+  hotelurl:'http://www.wtaipei.com/'
+  cityurl:'http://en.wikipedia.org/wiki/Taipei'
 )
 
 Itinerary.create(
@@ -67,6 +79,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Seoul Travel Lodge',
   tfd: 180
+  hotelurl:'http://www.travelodge.com/hotels/south-korea/seoul/search-results'
+  cityurl:'http://en.wikipedia.org/wiki/Seoul'
 )
 
 Itinerary.create(
@@ -76,6 +90,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'JJ Guest House',
   tfd: 180
+  hotelurl:'http://english.visitkorea.or.kr/enu/SI/SI_EN_3_1_1_1.jsp?cid=1830752'
+  cityurl:'http://en.wikipedia.org/wiki/Seoul'
 )
 
 Itinerary.create(
@@ -85,6 +101,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Summit Hotel',
   tfd: 180
+  hotelurl:'http://www.summithotelseoul.com/_KOR/index/index.asp'
+  cityurl:'http://en.wikipedia.org/wiki/Seoul'
 )
 
 Itinerary.create(
@@ -94,6 +112,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Koreana Hotel',
   tfd: 180
+  hotelurl:'http://www.koreanahotel.com/2013_home/main/index.asp'
+  cityurl:'http://en.wikipedia.org/wiki/Seoul'
 )
 
 Itinerary.create(
@@ -103,6 +123,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Lotte Hotel',
   tfd: 180
+  hotelurl:'http://www.lottehotel.com/seoul/en/'
+  cityurl:'http://en.wikipedia.org/wiki/Singapore'
 )
 
 Itinerary.create(
@@ -112,6 +134,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Fragrance Hotel',
   tfd: 240
+  hotelurl:'http://www.fragrancehotel.com/'
+  cityurl:'http://en.wikipedia.org/wiki/Singapore'
 )
 
 Itinerary.create(
@@ -121,6 +145,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Tresor Tavern',
   tfd: 240
+  hotelurl:'http://tresortavern.com/'
+  cityurl:'http://en.wikipedia.org/wiki/Singapore'
 )
 
 Itinerary.create(
@@ -130,6 +156,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Duxton Hotel',
   tfd: 240
+  hotelurl:'http://www.theduxton.sg/abouthotel-en.html'
+  cityurl:'http://en.wikipedia.org/wiki/Singapore'
 )
 
 Itinerary.create(
@@ -139,6 +167,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Scarlet Hotel Singapore',
   tfd: 240
+  hotelurl:'http://www.thescarlethotels.com/singapore/?src=google_cpc_brand_local'
+  cityurl:'http://en.wikipedia.org/wiki/Singapore'
 )
 
 Itinerary.create(
@@ -148,6 +178,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Raffles Hotel Singapore',
   tfd: 240
+  hotelurl:'http://www.raffles.com/singapore/'
+  cityurl:'http://en.wikipedia.org/wiki/Singapore'
 )
 
 Itinerary.create(
@@ -157,6 +189,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Hotel Arma',
   tfd: 360
+  hotelurl:'http://www.hotelarma.com/'
+  cityurl:'http://en.wikipedia.org/wiki/Mumbai'
 )
 
 Itinerary.create(
@@ -166,6 +200,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Hotel Mumbai',
   tfd: 360
+  hotelurl: 'http://ww38.bentleys-hotel.com/'
+  cityurl:'http://en.wikipedia.org/wiki/Mumbai'
 )
 
 Itinerary.create(
@@ -175,6 +211,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Leela Hotel',
   tfd: 360
+  hotelurl:'http://www.theleela.com/locations/mumbai'
+  cityurl:'http://en.wikipedia.org/wiki/Mumbai'
 )
 
 Itinerary.create(
@@ -184,6 +222,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'VITS Hotel',
   tfd: 360
+  hotelurl:'http://www.vitshotels.com/'
+  cityurl:'http://en.wikipedia.org/wiki/Mumbai'
 )
 
 Itinerary.create(
@@ -193,6 +233,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Hyatt Regency Mumbai Hotel',
   tfd: 360
+  hotelurl:'http://mumbai.grand.hyatt.com/en/hotel/home.html'
+  cityurl:'http://en.wikipedia.org/wiki/Mumbai'
 )
 
 Itinerary.create(
@@ -202,6 +244,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Route 39 Hostel',
   tfd: 540
+  hotelurl:'http://route39istanbul.com/istanbul/'
+  cityurl:'http://en.wikipedia.org/wiki/Istanbul'
 )
 
 Itinerary.create(
@@ -211,6 +255,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Codial House Hotel',
   tfd: 540
+  hotelurl:'https://www.cordialhouse.com/'
+  cityurl:'http://en.wikipedia.org/wiki/Istanbul'
 )
 
 Itinerary.create(
@@ -220,6 +266,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Ahmet Affendi Hotel',
   tfd: 540
+  hotelurl:'http://www.ahmetefendievi.com/'
+  cityurl:'http://en.wikipedia.org/wiki/Istanbul'
 )
 
 Itinerary.create(
@@ -229,6 +277,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Boutique Saint Sophia',
   tfd: 540
+  hotelurl:'http://boutiquesaintsophia.com/'
+  cityurl:'http://en.wikipedia.org/wiki/Istanbul'
 )
 
 Itinerary.create(
@@ -238,6 +288,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'W Istanbul Hotel',
   tfd: 540
+  hotelurl:'http://www.wistanbul.com.tr/'
+  cityurl:'http://en.wikipedia.org/wiki/Istanbul'
 )
 
 Itinerary.create(
@@ -247,6 +299,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Avenue Inn Hotel',
   tfd: 720
+  hotelurl:'http://www.avenue-restaurant.co.uk/'
+  cityurl:'http://en.wikipedia.org/wiki/London'
 )
 
 Itinerary.create(
@@ -256,6 +310,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Marble Arch Inn Hotel',
   tfd: 720
+  hotelurl:'http://www.marblearch-inn.co.uk/'
+  cityurl:'http://en.wikipedia.org/wiki/London'
 )
 
 Itinerary.create(
@@ -265,6 +321,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Byron Hotel',
   tfd: 720
+  hotelurl:'http://www.capricornhotels.co.uk/hotel_byron/'
+  cityurl:'http://en.wikipedia.org/wiki/London'
 )
 
 Itinerary.create(
@@ -274,6 +332,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Sloan Square Hotel',
   tfd: 720
+  hotelurl:'http://www.sloanesquarehotel.co.uk/default-en.html'
+  cityurl:'http://en.wikipedia.org/wiki/London'
 )
 
 Itinerary.create(
@@ -283,6 +343,8 @@ Itinerary.create(
   starting: 'Hong Kong',
   hotel: 'Claridges Hotel',
   tfd: 720
+  hotelurl:'http://www.claridges.co.uk/'
+  cityurl:'http://en.wikipedia.org/wiki/London'
 )
 
 
